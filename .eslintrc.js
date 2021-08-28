9bfb3e86ba9fd4941734b54d8bfed056930f9dc0
maxbarsukov <maximbarsukov@bk.ru>
2021-08-28 11:29:45 +0300

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'arrow-parens': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'no-unused-vars': 'warn',
+    'space-before-function-paren': 0,
+    'no-underscore-dangle': 'warn',
+    'max-len': ['warn', 100, 2, { ignoreUrls: true }],
+    'no-console': 'off',
+    'no-alert': 'warn',
+
+    'no-param-reassign': 'off',
+    'prettier/prettier': ['warn'],
+  },
+};
