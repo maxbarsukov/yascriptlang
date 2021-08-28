58f78c9b3488dab828dec439f5e7fb7b1a820d81
maxbarsukov <maximbarsukov@bk.ru>
2021-08-28 12:21:45 +0300

    Little fix

--- bin/yascriptlang.js
+++ bin/yascriptlang.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import say from '../src/index'
+import say from '../src/index.js'
 
 const args = process.argv.splice(process.execArgv.length + 2);
 const name = args[0];
 
-say('Hello ', name);
+say('Hello ' + name);
