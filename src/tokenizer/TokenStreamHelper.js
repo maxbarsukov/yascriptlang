9bd028a09245bd3d79b45935df95d5b3ad03dfef
maxbarsukov <maximbarsukov@bk.ru>
2021-08-30 13:46:05 +0300

    Separate TokenStreamHelper from TokenStream

---
+++ src/tokenizer/TokenStreamHelper.js
@@ -0,0 +1,28 @@
+import Keywords from './Keywords.js';
+
+const TokenStreamHelper = {
+  keywords: Object.values(Keywords),
+  isWhitespace(ch) {
+    return ' \t\n'.includes(ch);
+  },
+  isDigit(ch) {
+    return /[0-9]/i.test(ch);
+  },
+  isIdStart(ch) {
+    return /[a-zλ_]/i.test(ch);
+  },
+  isId(ch) {
+    return this.isIdStart(ch) || '?!-<>=0123456789'.includes(ch);
+  },
+  isKeyword(x) {
+    return this.keywords.includes(x);
+  },
+  isPunc(ch) {
+    return ',;(){}[]'.includes(ch);
+  },
+  isOpChar(ch) {
+    return '+-*/%=&|<>!^'.includes(ch);
+  },
+};
+
+export default TokenStreamHelper;
