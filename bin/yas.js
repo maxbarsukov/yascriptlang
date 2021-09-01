4a5545e44319c202776b458e2ff552b1e7369997
maxbarsukov <maximbarsukov@bk.ru>
2021-09-01 14:09:00 +0300

    Fix scripts codestyle from bin

--- bin/yas.js
+++ bin/yas.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { run } from '../src/index.js';
 import fs from 'fs';
 import path from 'path';
+import { run } from '../src/index.js';
 
-const [,, ...args] = process.argv;
-const filePath = `${process.cwd()}${path.sep}${args}`
+const [, , ...args] = process.argv;
+const filePath = `${process.cwd()}${path.sep}${args}`;
 
 fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   if (err) {
-    console.error(err)
+    console.error(err);
     return;
   }
   run(data, result => {
     console.log('Result: ', result);
   });
-})
+});
--- bin/yasjs.js
+++ bin/yasjs.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import { compile, runJS } from '../src/index.js';
 import fs from 'fs';
 import path from 'path';
+import { compile, runJS } from '../src/index.js';
 
-const [,, ...args] = process.argv;
-const filePath = `${process.cwd()}${path.sep}${args}`
+const [, , ...args] = process.argv;
+const filePath = `${process.cwd()}${path.sep}${args}`;
 
 fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
   if (err) {
-    console.error(err)
+    console.error(err);
     return;
   }
   runJS(compile(data));
